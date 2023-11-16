665cf6f44f58d8fa7119f505649941b3d4a95327
Siddharth Shah <siddharthsshah1207@gmail.com>
2023-11-15 22:23:01 -0500

    Update ZoneUI.java

--- src/wolf_parking_system/menu/ZoneUI.java
+++ src/wolf_parking_system/menu/ZoneUI.java
@@ -3,9 +3,85 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.sql.SQLException;
 
+import wolf_parking_system.crud.ZoneCRUD;
+
 
 public class ZoneUI {
     public static void zoneUI(BufferedReader reader) throws NumberFormatException, IOException, SQLException {
+
+        String ZoneID, LotName;
+
+        String[] args;
+        boolean exit_val = true;
+        String[] main_args = null;
+        while (exit_val) {
+
+        System.out.println("1. Enter Zone Information");
+        System.out.println("2. Update Zone Information");
+        System.out.println("3. Delete Zone Information");
+        System.out.println("4. View Zone Information");
     
+
+        String input = reader.readLine();
+
+        switch(Integer.parseInt(input)){
+            case 1:
+                System.out.println("Enter | separated String ZoneID,String LotName");
+                args = reader.readLine().split("[|]");
+                ZoneID = args[0];
+                LotName = args[1];
+
+
+//                BookCRUD.insertBook(PID, PublicationDate, ISBN, Edition);
+                if (ZoneCRUD.enterZoneInfo(ZoneID,LotName)) {
+                    System.out.println("Operation Successful");
+                } else {
+                    System.out.println("Operation Failed");
+                }
+                return;
+
+            case 2:
+                System.out.println("Enter | separated String ZoneID,String LotName");
+                args = reader.readLine().split("[|]");
+                ZoneID = args[0];
+                LotName = args[1];
+
+                if (ZoneCRUD.updateZoneInfo(ZoneID,LotName)) {
+                    System.out.println("Operation Successful");
+                } else {
+                    System.out.println("Operation Failed");
+                }
+                return;
+
+            case 3:
+                System.out.println("Enter | separated String ZoneID,String LotName");
+                args = reader.readLine().split("[|]");
+                ZoneID = args[0];
+                LotName = args[1];
+
+                if (ZoneCRUD.deleteZoneInfo(ZoneID,LotName)) {
+                    System.out.println("Operation Successful");
+                } else {
+                    System.out.println("Operation Failed");
+                }
+                return;
+            case 4:
+                // CHECK IF YOU WANT TO DISPLAY ALL (ZONEID,LOTNAME) TUPLES OR ALL ZONEIDS FOR A PARKING LOT
+                if (!ZoneCRUD.viewZone().isEmpty()) {
+                    System.out.println("Operation Successful");
+                } else {
+                    System.out.println("Operation Failed");
+                }
+                return;
+            default:
+                System.out.println("Enter a valid choice");
+
+
+        }
+
+        System.out.println("Enter a valid choice");
+
+    }
+
     }
 }
