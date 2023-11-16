35ce82c5486327976da0acaf79c68badf5d20995
Rishabh Bhargava <69590253+Bullet1403@users.noreply.github.com>
2023-11-16 00:55:03 -0500

    Update DriverUI.java

--- src/wolf_parking_system/menu/DriverUI.java
+++ src/wolf_parking_system/menu/DriverUI.java
@@ -3,49 +3,88 @@ package wolf_parking_system.menu;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.sql.SQLException;
+import wolf_parking_system.crud.DriverCRUD;
+import wolf_parking_system.crud.ZoneCRUD;
+import wolf_parking_system.crud.DriverCRUD;
 
 public class DriverUI {
-      public static void driverUI(BufferedReader reader) throws NumberFormatException, IOException, SQLException {
-            String[] main_args = null;
-            boolean exit_val = true;
-            while (exit_val) {
-                  System.out.println("Driver operations");
-                  System.out.println("1. Add Driver Info");
-                  System.out.println("2. Update Driver Info");
-                  System.out.println("3. Delete Driver Info");
-                  System.out.println("4. View Driver Info");
-                  //System.out.println("5. Staff Payment Operations");
-                  //System.out.println("6. Reports");
-                  System.out.println("7. Back to Main Menu");
+    public static void driverUI(BufferedReader reader) throws NumberFormatException, IOException, SQLException {
+        Long DriverID;
+        String Name, Status;
+        Boolean Handicap;
 
-                  String input = reader.readLine();
-                  String[] str = null;
+        String[] main_args = null;
+        String [] args = null;
+        boolean exit_val = true;
 
-                  switch (Integer.parseInt(input)) {
-                  case 1:
-                        PublicationUI.publicationUI(reader);
-                        break;
-                  case 2:
-                        StaffUI.staffUI(reader);
-                        break;
-                  case 3:
-                        DistributorOperations.distributorOperationsUI(reader);
-                        break;
-                  case 4:
-                        DistributorUI.distributorUI(reader);
-                        break;
-                  case 5:
-                        PaymentUI.paymentUI(reader);
-                        break;
-                  case 6:
-                        ReportUI.reportUI(reader);
-                        break;
-                  case 7:
-                        Main.main(str);
-                  default:
-                        System.out.println("Enter a valid choice");
+
+        while (exit_val) {
+            System.out.println("Driver operations");
+            System.out.println("1. Add Driver Info");
+            System.out.println("2. Update Driver Info");
+            System.out.println("3. Delete Driver Info");
+            System.out.println("4. View Driver Info");
+            System.out.println("5. Back to Main Menu");
+
+            String input = reader.readLine();
+
+            switch (Integer.parseInt(input)) {
+                case 1:
+                  System.out.println("Enter | separated Long DriverID, String Name, Boolean Handicap, String Status");
+                  args = reader.readLine().split("[|]");
+                  DriverID = Long.valueOf(args[0]);
+                  Name = args[1];
+                  Handicap = Boolean.valueOf(args[2]);
+                  Status = args[3];
+
+                  if (DriverCRUD.enterDriverInfo(DriverID, Name, Handicap, Status)) {
+                    System.out.println("Operation Successful");
+                  } else {
+                        System.out.println("Operation Failed");
                   }
+                  break;
+                case 2:
+                  System.out.println("Enter | separated Long DriverID, String Name, Boolean Handicap, String Status");
+                  args = reader.readLine().split("[|]");
+                  DriverID = Long.valueOf(args[0]);
+                  Name = args[1];
+                  Handicap = Boolean.valueOf(args[2]);
+                  Status = args[3];
 
+                  if (DriverCRUD.updateDriverInfo(DriverID, Name, Handicap, Status)) {
+                    System.out.println("Operation Successful");
+                  } else {
+                        System.out.println("Operation Failed");
+                  }
+                  break;
+                case 3:
+                  System.out.println("Enter | separated Long DriverID, String Name, Boolean Handicap, String Status");
+                  args = reader.readLine().split("[|]");
+                  DriverID = Long.valueOf(args[0]);
+                  Name = args[1];
+                  Handicap = Boolean.valueOf(args[2]);
+                  Status = args[3];
+
+                  if (DriverCRUD.deleteDriverInfo(DriverID)) {
+                    System.out.println("Operation Successful");
+                  } else {
+                        System.out.println("Operation Failed");
+                  }
+                  break;
+
+                case 4:
+                  if (!DriverCRUD.viewDriver().isEmpty()) {
+                    System.out.println("Operation Successful");
+                  } else {
+                    System.out.println("Operation Failed");
+                  } 
+
+                default:
+                  System.out.println("Enter a valid choice");
+                  break;
+            
             }
-      }
+
+        }
+    }
 }
